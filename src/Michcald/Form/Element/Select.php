772ab50864f547d4e1d9763f359bc56ed96cfc87
Michael <michcald@gmail.com>
2014-03-09 20:33:09 +0000

    Adding select element

---
+++ src/Michcald/Form/Element/Select.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Michcald\Form\Element;
+
+class Select extends \Michcald\Form\Element
+{
+    private $options = array();
+    
+    public function addOption($name, $value)
+    {
+        $this->options[$name] = $value;
+
+        return $this;
+    }
+    
+    public function __toString()
+    {
+        $str = '<select ' .
+            'name="' . $this->getName() . '" ' .
+            $this->getAttributesString() .
+            '>';
+        
+        foreach ($this->options as $label => $value) {
+            $str .= '<option value="' . $value . '"';
+            if ($value == $this->getValue()) {
+                $str .= ' selected';
+            }
+            $str .= '>' . $label;
+            $str .= '</option>';
+        }
+        
+        $str .= '</select>';
+        
+        return $str;
+    }
+}
