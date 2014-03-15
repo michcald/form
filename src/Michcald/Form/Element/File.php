933d49d85c334d5005615dd2288034c865551ec1
Michael <michcald@gmail.com>
2014-03-15 21:46:50 +0000

    Overriding filter and isValid for file input

--- src/Michcald/Form/Element/File.php
+++ src/Michcald/Form/Element/File.php
@@ -5,6 +5,32 @@ namespace Michcald\Form\Element;
 class File extends \Michcald\Form\Element
 {
 
+    public function getValue($filtered = true)
+    {
+        $filePath = $this->value['tmp_name'];
+
+        if ($filtered) {
+            foreach ($this->filters as $filter) {
+                $filter->filter($filePath);
+            }
+        }
+
+        return $this->value;
+    }
+
+    public function isValid()
+    {
+        $filePath = $this->value['tmp_name'];
+
+        foreach ($this->validators as $validator) {
+            if (!$validator->validate($filePath)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public function __toString()
     {
         return '<input ' .
